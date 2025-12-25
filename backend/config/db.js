dc31b997f2cca2ad508128477b6b22033f8e0b28
PRAJAPATI VANSH <prajapativansh512@gmail.com>
2025-12-25 15:36:10 +0530

    Remove deprecated mongoose options

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,7 @@ import { logger } from '../utils/logger.js';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     logger.info(`MongoDB Connected: ${conn.connection.host}`);
 
